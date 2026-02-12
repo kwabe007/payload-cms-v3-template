e4c589af221edea5296206669a059f137be6a2eb
Kwabena <kwabena.a92@gmail.com>
2026-02-12 15:38:21 +0100

    Set image sizes and static dir for media

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -12,5 +12,34 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
-  upload: true,
+  upload: {
+    staticDir: 'media',
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 320,
+        height: 320,
+      },
+      {
+        name: 'w600',
+        width: 600,
+        // When height is undefined, it is calculated automatically based on width while maintaining
+        // the original aspect ratio.
+      },
+      {
+        name: 'w1080',
+        width: 1080,
+        // When height is undefined, it is calculated automatically based on width while maintaining
+        // the original aspect ratio.
+      },
+      {
+        name: 'w1920',
+        width: 1920,
+        // When height is undefined, it is calculated automatically based on width while maintaining
+        // the original aspect ratio.
+      },
+    ],
+    adminThumbnail: 'thumbnail',
+    mimeTypes: ['image/*'],
+  },
 }
