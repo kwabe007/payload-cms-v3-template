bea72081715a052b0b90d664e4688c8f6b207980
Kwabena <kwabena.a92@gmail.com>
2026-06-04 16:14:39 +0200

    Format

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,41 +1,41 @@
-import type { CollectionConfig } from 'payload'
+import type { CollectionConfig } from "payload";
 
 export const Media: CollectionConfig = {
-  slug: 'media',
+  slug: "media",
   access: {
     read: () => true,
   },
   fields: [
     {
-      name: 'alt',
-      type: 'text',
+      name: "alt",
+      type: "text",
       admin: {
         description:
-          'The alt attribute is a short text description of an image that helps screen readers convey its meaning and appears if the image fails to load.',
+          "The alt attribute is a short text description of an image that helps screen readers convey its meaning and appears if the image fails to load.",
       },
     },
   ],
   upload: {
-    staticDir: 'media',
+    staticDir: "media",
     imageSizes: [
       {
-        name: 'thumbnail_256',
+        name: "thumbnail_256",
         width: 256,
       },
       {
-        name: 'tablet_768',
+        name: "tablet_768",
         width: 768,
       },
       {
-        name: 'large_1280',
+        name: "large_1280",
         width: 1280,
       },
       {
-        name: 'desktop_1920',
+        name: "desktop_1920",
         width: 1920,
       },
     ],
-    adminThumbnail: 'thumbnail',
-    mimeTypes: ['image/*', 'video/*'],
+    adminThumbnail: "thumbnail",
+    mimeTypes: ["image/*", "video/*"],
   },
-}
+};
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,15 +1,15 @@
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
-import sharp from 'sharp'
+import { postgresAdapter } from "@payloadcms/db-postgres";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import { fileURLToPath } from "url";
+import sharp from "sharp";
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default buildConfig({
   admin: {
@@ -20,15 +20,15 @@ export default buildConfig({
   },
   collections: [Users, Media],
   editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
+  secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
+    outputFile: path.resolve(dirname, "payload-types.ts"),
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URL || '',
+      connectionString: process.env.DATABASE_URL || "",
     },
   }),
   sharp,
   plugins: [],
-})
+});
