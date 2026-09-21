eebd80915ccdc9d4532ad0f436d142e163a72f6c
Kwabena <kwabena.a92@gmail.com>
2026-09-21 16:53:58 +0200

    Update payload types

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -130,6 +130,7 @@ export interface User {
   resetPasswordExpiration?: string | null;
   salt?: string | null;
   hash?: string | null;
+  resetPasswordRequestedAt?: string | null;
   loginAttempts?: number | null;
   lockUntil?: string | null;
   sessions?:
@@ -284,6 +285,7 @@ export interface UsersSelect<T extends boolean = true> {
   resetPasswordExpiration?: T;
   salt?: T;
   hash?: T;
+  resetPasswordRequestedAt?: T;
   loginAttempts?: T;
   lockUntil?: T;
   sessions?:
